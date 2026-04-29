22fd06d7701547dc716c8bf70c8d9e29d562baa4
jean van <van@offramp.xyz>
2026-04-29 08:45:48 +0800

    add error state to date input component

--- apps/web/src/components/input/DateInput.tsx
+++ apps/web/src/components/input/DateInput.tsx
@@ -3,13 +3,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 import { COLORS } from '../../styles/colors';
 import { forwardRef } from 'react';
 import { addDate } from '../../utils/helper/addDate';
-
-interface DateInputProps {
-    onChangeStartDate?: (date: Date) => void;
-    startDate?: Date;
-    onChangeEndDate?: (date: Date) => void;
-    endDate?: Date;
-}
+import { SmallNote } from '../Text/SmallNote';
 
 enum DateType {
     START,
@@ -20,10 +14,11 @@ interface CustomInputProps {
     value?: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
     type: DateType;
+    isError?: boolean;
 }
 
 const DatePickerButton = forwardRef<HTMLButtonElement, CustomInputProps>(
-    ({ value, onClick, type }, ref) => (
+    ({ value, onClick, type, isError }, ref) => (
         <button
             ref={ref}
             onClick={onClick}
@@ -32,7 +27,10 @@ const DatePickerButton = forwardRef<HTMLButtonElement, CustomInputProps>(
                 padding: '5px 10px',
                 borderRadius: '7px',
                 backgroundColor: 'transparent',
-                border: `1px solid ${COLORS.skintone}`,
+                border: isError
+                    ? '1px solid red'
+                    : `1px solid ${COLORS.skintone}`,
+                color: isError ? 'red' : 'black',
             }}
         >
             {value || (type === DateType.START ? 'Start date' : 'End date')}
@@ -40,53 +38,93 @@ const DatePickerButton = forwardRef<HTMLButtonElement, CustomInputProps>(
     ),
 );
 
+interface DateInputProps {
+    onChangeStartDate?: (date: Date) => void;
+    startDate?: Date;
+    onChangeEndDate?: (date: Date) => void;
+    endDate?: Date;
+    error?: string[];
+}
+
 export const DateInput = ({
     onChangeStartDate,
     startDate,
     onChangeEndDate,
     endDate,
+    error,
 }: DateInputProps) => {
+    const isError = (error?.length || 0) >= 1;
+
     return (
         <div
             style={{
                 display: 'flex',
                 flexDirection: 'column',
-                borderBottom: `2px solid ${COLORS.skintone}`,
-                paddingBottom: '10px',
-                gap: '5px',
                 width: '100%',
+                gap: '4px',
             }}
         >
-            <span>Select dates:</span>
             <div
                 style={{
                     display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    gap: '4px',
+                    flexDirection: 'column',
+                    borderBottom: isError
+                        ? '2px solid red'
+                        : `2px solid ${COLORS.skintone}`,
+                    paddingBottom: '10px',
+                    gap: '5px',
+                    width: '100%',
                 }}
             >
-                <DatePicker
-                    maxDate={endDate ? addDate(endDate, -1) : undefined}
-                    selected={startDate}
-                    onChange={(date: Date | null) => {
-                        if (date) {
-                            onChangeStartDate?.(date);
-                        }
+                <span style={{ color: isError ? 'red' : 'black' }}>
+                    Select dates:
+                </span>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        gap: '4px',
                     }}
-                    customInput={<DatePickerButton type={DateType.START} />}
-                />
-                <DatePicker
-                    minDate={startDate ? addDate(startDate, 1) : undefined}
-                    selected={endDate}
-                    onChange={(date: Date | null) => {
-                        if (date) {
-                            onChangeEndDate?.(date);
+                >
+                    <DatePicker
+                        maxDate={endDate ? addDate(endDate, -1) : undefined}
+                        selected={startDate}
+                        onChange={(date: Date | null) => {
+                            if (date) {
+                                onChangeStartDate?.(date);
+                            }
+                        }}
+                        customInput={
+                            <DatePickerButton
+                                type={DateType.START}
+                                isError={isError}
+                            />
                         }
-                    }}
-                    customInput={<DatePickerButton type={DateType.END} />}
-                />
+                    />
+                    <DatePicker
+                        minDate={startDate ? addDate(startDate, 1) : undefined}
+                        selected={endDate}
+                        onChange={(date: Date | null) => {
+                            if (date) {
+                                onChangeEndDate?.(date);
+                            }
+                        }}
+                        customInput={
+                            <DatePickerButton
+                                type={DateType.END}
+                                isError={isError}
+                            />
+                        }
+                    />
+                </div>
             </div>
+            {isError &&
+                error?.map((error, i) => (
+                    <SmallNote key={i} style={{ color: 'red' }}>
+                        * {error}
+                    </SmallNote>
+                ))}
         </div>
     );
 };
