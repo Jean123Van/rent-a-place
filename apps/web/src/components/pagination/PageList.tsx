b1215c80870dfbaae7d942e9eeb8acedf2916fdb
jean van <jeanvaningemar.tolentino@gmail.com>
2026-06-07 23:44:58 +0800

    create reusable component for pagination

---
+++ apps/web/src/components/pagination/PageList.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+
+interface PageListProps {
+    currentPage: number;
+    onChange: (page: number) => void;
+    totalPages: number;
+}
+
+export const PageList = ({
+    totalPages,
+    currentPage,
+    onChange,
+}: PageListProps) => {
+    const [renderedPageNum, setRenderedPageNum] = useState<number | undefined>(
+        totalPages > 10 ? 10 : totalPages,
+    );
+
+    return (
+        <div
+            style={{
+                color: 'white',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                gap: '5px',
+            }}
+        >
+            {renderedPageNum! > 10 && (
+                <span
+                    style={{
+                        fontSize: '12px',
+                        textDecoration: 'underline',
+                        cursor: 'pointer',
+                    }}
+                    onClick={() => {
+                        setRenderedPageNum((prev) => {
+                            if (!prev) return;
+
+                            return prev % 10 > 0
+                                ? prev - (prev % 10)
+                                : prev - 10;
+                        });
+                    }}
+                >
+                    prev...
+                </span>
+            )}
+            {Array.from(
+                { length: renderedPageNum! > 10 ? 10 : renderedPageNum! },
+                (_, i) =>
+                    i +
+                    (renderedPageNum! >= 10
+                        ? renderedPageNum! - 9
+                        : renderedPageNum!),
+            ).map((pageNum) => (
+                <span
+                    style={{
+                        fontSize: '12px',
+                        textDecoration: 'underline',
+                        cursor: 'pointer',
+                        color: pageNum === currentPage ? 'grey' : 'white',
+                    }}
+                    onClick={() => {
+                        onChange(pageNum);
+                    }}
+                >
+                    {pageNum}
+                </span>
+            ))}
+            {totalPages > renderedPageNum! && (
+                <span
+                    style={{
+                        textDecoration: 'underline',
+                        fontSize: '12px',
+                        cursor: 'pointer',
+                    }}
+                    onClick={() => {
+                        setRenderedPageNum((prev) => {
+                            if (!prev) return;
+
+                            const remainingPages = totalPages - prev;
+
+                            return remainingPages > 10
+                                ? prev + 10
+                                : prev + remainingPages;
+                        });
+                    }}
+                >
+                    next...
+                </span>
+            )}
+        </div>
+    );
+};
--- apps/web/src/pages/VendorProducts.tsx
+++ apps/web/src/pages/VendorProducts.tsx
@@ -4,7 +4,8 @@ import type { ProductData } from '../utils/types';
 import { LoadingSpinner } from '../components/Animation/LoadingSpinner/LoadingSpinner';
 import { ListContainer } from '../components/container/ListContainer';
 import { ProductListTile } from '../components/container/ProductListTile';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { PageList } from '../components/pagination/PageList';
 
 export const VendorProducts = () => {
     const [currentPage, setCurrentPage] = useState(1);
@@ -13,23 +14,6 @@ export const VendorProducts = () => {
         queryKey: ['vendor', 'products', currentPage],
         queryFn: () => getAllProducts(currentPage),
     });
-
-    const [renderedPageNum, setRenderedPageNum] = useState<number | undefined>(
-        undefined,
-    );
-
-    useEffect(() => {
-        if (!data || renderedPageNum) return;
-
-        const totalPages = data?.data.totalPages;
-
-        if (totalPages > 10) {
-            setRenderedPageNum(10);
-            return;
-        }
-
-        setRenderedPageNum(data?.data.totalPages);
-    }, [data]);
 
     if (isLoading) {
         return (
@@ -51,82 +35,13 @@ export const VendorProducts = () => {
             {data?.data.products.map((product: ProductData) => (
                 <ProductListTile product={product} key={product.id} />
             ))}
-            <div
-                style={{
-                    color: 'white',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    gap: '5px',
+            <PageList
+                currentPage={currentPage}
+                onChange={(page) => {
+                    setCurrentPage(page);
                 }}
-            >
-                {renderedPageNum! > 10 && (
-                    <span
-                        style={{
-                            fontSize: '12px',
-                            textDecoration: 'underline',
-                            cursor: 'pointer',
-                        }}
-                        onClick={() => {
-                            setRenderedPageNum((prev) => {
-                                if (!prev) return;
-
-                                return prev % 10 > 0
-                                    ? prev - (prev % 10)
-                                    : prev - 10;
-                            });
-                        }}
-                    >
-                        prev...
-                    </span>
-                )}
-                {Array.from(
-                    { length: renderedPageNum! > 10 ? 10 : renderedPageNum! },
-                    (_, i) =>
-                        i +
-                        (renderedPageNum! >= 10
-                            ? renderedPageNum! - 9
-                            : renderedPageNum!),
-                ).map((pageNum) => (
-                    <span
-                        style={{
-                            fontSize: '12px',
-                            textDecoration: 'underline',
-                            cursor: 'pointer',
-                            color: pageNum === currentPage ? 'grey' : 'white',
-                        }}
-                        onClick={() => {
-                            setCurrentPage(pageNum);
-                        }}
-                    >
-                        {pageNum}
-                    </span>
-                ))}
-                {data?.data.totalPages > renderedPageNum! && (
-                    <span
-                        style={{
-                            textDecoration: 'underline',
-                            fontSize: '12px',
-                            cursor: 'pointer',
-                        }}
-                        onClick={() => {
-                            setRenderedPageNum((prev) => {
-                                if (!prev) return;
-
-                                const totalPages = data?.data.totalPages;
-
-                                const remainingPages = totalPages - prev;
-
-                                return remainingPages > 10
-                                    ? prev + 10
-                                    : prev + remainingPages;
-                            });
-                        }}
-                    >
-                        next...
-                    </span>
-                )}
-            </div>
+                totalPages={data?.data.totalPages}
+            />
         </ListContainer>
     );
 };
